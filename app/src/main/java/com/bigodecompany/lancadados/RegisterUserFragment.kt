53f835ff4d09fc65424ee2eb0b6633542c410b9a
fwtrodrigo <fwtrodrigo@gmail.com>
2021-06-12 11:15:54 -0300

    migracao do registro de usuario de activity para fragment

---
+++ app/src/main/java/com/bigodecompany/lancadados/RegisterUserFragment.kt
@@ -0,0 +1,35 @@
+package com.bigodecompany.lancadados
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bigodecompany.lancadados.databinding.FragmentRegisterUserBinding
+
+class RegisterUserFragment : Fragment() {
+    private var binding: FragmentRegisterUserBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentRegisterUserBinding.inflate(inflater, container, false)
+
+        val edtPlayerName = binding?.edtPlayerName
+        val btnRegister = binding?.btnRegister
+
+        btnRegister?.setOnClickListener {
+            val playerName = edtPlayerName?.text.toString()
+
+            btnRegister.setOnClickListener {
+                MainActivity.open(requireContext(), playerName)
+            }
+        }
+
+
+        return binding?.root
+    }
+
+}
